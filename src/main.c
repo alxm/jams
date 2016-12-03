d8398f6b2a64097630abbaffd82da6f1c70f8ab7
Alex Margarit <alex@alxm.org>
2016-12-02 18:10:22 -0800

    Initial commit.

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <a2x.h>
+
+A_SETUP
+{
+    a_settings_set("app.title", "church2");
+    a_settings_set("app.version", "1.0");
+    a_settings_set("app.author", "alex");
+    a_settings_set("app.output.on", "yes");
+}
+
+A_MAIN
+{
+    printf("Code me!\n");
+}
