8fb253acb3ea34298e8811f3638c2224a55a43dd
Alex Margarit <alex@alxm.org>
2017-03-05 14:16:16 -0800

    Initial commit: Space Station Plunder (SSP) - A roguelike game made for 7DRL 2017!

---
+++ README.md
@@ -0,0 +1,12 @@
+Space Station Plunder
+=====================
+
+A roguelike game made for 7DRL 2017! Development happens March 5th-12th.
+
+License
+-------
+
+Copyright 2017 Alex Margarit (alex@alxm.org)
+
+* Code licensed under [GNU GPL3](https://www.gnu.org/licenses/gpl.html) (see `COPYING`)
+* Graphics licensed under [CC BY-NC-ND 4.0](https://creativecommons.org/licenses/by-nc-nd/4.0/) (see `CC-BY-NC-ND`)
---
+++ src/main.c
@@ -0,0 +1,33 @@
+/*
+    Copyright 2017 Alex Margarit <http://www.alxm.org/>
+    Space Station Plunder (SSP) - A roguelike game made for 7DRL 2017!
+
+    SSP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SSP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SSP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <a2x.h>
+
+A_SETUP
+{
+    a_settings_set("app.title", "Space Station Plunder");
+    a_settings_set("app.version", "0.0");
+    a_settings_set("app.author", "alxm");
+    a_settings_set("app.output.on", "yes");
+    a_settings_set("app.output.verbose", "yes");
+}
+
+A_MAIN
+{
+    printf("Code me!\n");
+}
